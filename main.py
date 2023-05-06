8cf9d9ccd71957cd700765880775ccf9f58a96b4
sweety-cool <sweety127ahmed@gmail.com>
2023-05-06 19:50:51 +0530

    chnages in app

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from flask import Flask
+import os
+
 
 app = Flask(__name__)
-
+port = int(os.environ.get("PORT", 8080))
 
 @app.route("/")
 def main():
@@ -9,4 +11,4 @@ def main():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=8080)
+    app.run(debug=True, port=port, host="0.0.0.0")
